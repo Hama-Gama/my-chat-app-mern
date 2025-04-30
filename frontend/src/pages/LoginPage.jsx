bb1207494818378b727f872605e076efba8afcd5
hama-dev <admiral3nelson7@gmail.com>
2025-04-30 08:52:31 +0500

    update json

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore'
+
+import {
+	Eye,
+	EyeOff,
+	Loader2,
+	Lock,
+	Mail,
+	MessageSquare,
+} from 'lucide-react'
+import { Link } from 'react-router-dom'
+import AuthImagePattern from '../components/AuthImagePattern'
+
+const LoginPage = () => {
+	const [showPassword, setShowPassword] = useState(false)
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	})
+	const { login, isLoggingIn } = useAuthStore()
+
+	const handleSubmit = async e => {
+		e.preventDefault()
+		login(formData)
+	}
+
+	return (
+		<div className='h-screen grid lg:grid-cols-2'>
+			{/* left side - Form */}
+			<div className='flex flex-col justify-center items-center p-6 sm:p-12'>
+				<div className='w-full max-w-md space-y-8'>
+					{/* LOGO */}
+					<div className='text-center mb-8'>
+						<div className='flex flex-col items-center gap-2 group'>
+							<div className='size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors'>
+								<MessageSquare className='size-6 text-primary' />
+							</div>
+							<h1 className='text-2xl font-bold mt-2'>
+								Рады снова видеть Вас!
+							</h1>
+							<div className='text-base-content/60'>Зайдите в свой аккаунт</div>
+						</div>
+					</div>
+					{/* FORM */}
+					<form onSubmit={handleSubmit} className='space-y-6'>
+						<div className='form-control'>
+							<label className='label'>
+								<span className='label-text font-medium mb-1.5'>Email</span>
+							</label>
+							<div className='relative'>
+								<div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+									<Mail className='size-5 text-base-content/40' />
+								</div>
+								<input
+									type='email'
+									className={`input input-bordered w-full pl-10`}
+									placeholder='hama-dev@gmail.com'
+									value={formData.email}
+									onChange={e =>
+										setFormData({ ...formData, email: e.target.value })
+									}
+								/>
+							</div>
+						</div>
+
+						<div className='form-control'>
+							<label className='label'>
+								<span className='label-text font-medium mb-1.5'>Пароль</span>
+							</label>
+							<div className='relative'>
+								<div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+									<Lock className='size-5 text-base-content/40' />
+								</div>
+								<input
+									type={showPassword ? 'text' : 'password'}
+									className={`input input-bordered w-full pl-10`}
+									placeholder='••••••••'
+									value={formData.password}
+									onChange={e =>
+										setFormData({ ...formData, password: e.target.value })
+									}
+								/>
+								<button
+									type='button'
+									className='absolute inset-y-0 right-0 pr-3 flex items-center'
+									onClick={() => setShowPassword(!showPassword)}
+								>
+									{showPassword ? (
+										<EyeOff className='size-5 text-base-content/40' />
+									) : (
+										<Eye className='size-5 text-base-content/40' />
+									)}
+								</button>
+							</div>
+						</div>
+
+						<button
+							type='submit'
+							className='btn btn-primary w-full'
+							disabled={isLoggingIn}
+						>
+							{isLoggingIn ? (
+								<>
+									<Loader2 className='size-5 animate-spin' />
+									Загрузка...
+								</>
+							) : (
+								'ВОЙТИ'
+							)}
+						</button>
+					</form>
+
+					<div className='text-center'>
+						<p className='text-base-content/60'>
+							Нет аккаунта ?{' '}
+							<Link to='/signup' className='link link-primary'>
+								Создать аккаунт
+							</Link>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			{/* right side */}
+
+			<AuthImagePattern
+				title='Наше сообщество'
+				subtitle='Находите новых друзей, оставляйте комментарии и будьте всегда на связи!'
+			/>
+		</div>
+	)
+}
+
+export default LoginPage
